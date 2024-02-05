74ee95d58b5b5882e33363e183870eb556dda4ab
Justin Arnold <justin.arnold@programmer.net>
2024-02-04 22:16:01 -0500

    Create very basic signup and login workflow

---
+++ pages/index.vue
@@ -0,0 +1,19 @@
+<script setup lang="ts">
+const user = useSupabaseUser()
+const supabase = useSupabaseClient()
+
+function logout() {
+    supabase.auth.signOut()
+    navigateTo('/login')
+}
+
+</script>
+
+<template>
+    <div class="h-screen w-screen overflow-hidden bg-slate-950 grid place-items-center">
+        <p class="text-white text-5xl">
+        Welcome, {{ user?.email }}
+        </p>
+        <button @click="logout()" class="btn btn-primary btn-outline flex-1">Logout</button>
+    </div>
+</template>
---
+++ pages/login.vue
@@ -0,0 +1,60 @@
+<script setup lang="ts">
+
+const email = ref('');
+const password = ref('');
+
+const supabase = useSupabaseClient()
+
+async function signUp() {
+    const { data, error } = await supabase.auth.signUp({
+        email: email.value,
+        password: password.value,
+    })
+
+    if (error) {
+        console.error('Error:', error)
+    } else {
+        console.log('Data:', data)
+    }
+}
+
+
+async function login() {
+    const { data,  error } = await supabase.auth.signInWithPassword({
+        email: email.value,
+        password: password.value,
+    })
+
+    if (error) {
+        console.error('Error:', error)
+    } else {
+        navigateTo('/')
+    }
+}
+
+</script>
+
+<template>
+    <div class="card bg-neutral w-[500px]">
+
+        <div class="card-body">
+            <div class="card-title">
+                <h1>Login</h1>
+            </div>
+            <form @submit.prevent="login">
+                <div class="form-control">
+                    <label for="email" class="label">Email</label>
+                    <input type="email" id="email" v-model="email" class="input" />
+                </div>
+                <div class="form-control">
+                    <label for="password" class="label">Password</label>
+                    <input type="password" id="password" v-model="password" class="input" />
+                </div>
+                <div class="form-control mt-8 flex-row gap-4">
+                    <button @click="signUp()" class="btn btn-primary btn-outline flex-1">Sign Up</button>
+                    <button type="submit" class="btn btn-primary flex-1">Login</button>
+                </div>
+            </form>
+        </div>
+    </div>
+</template>
