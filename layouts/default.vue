5d0702ff1cfd47184b1bde3d348ea96bfa711e96
Justin Arnold <justin.arnold@programmer.net>
2024-02-11 18:53:24 -0500

    feat: Update sidebar

--- layouts/default.vue
+++ layouts/default.vue
@@ -9,9 +9,12 @@ function logout() {
 
 <template>
     <div class="h-screen w-screen overflow-hidden bg-base-300 flex">
-        <div class="h-full w-40 bg-base-100 flex-shrink-0">
-            <div class="flex flex-col gap-4 p-4 h-full">
-                <NuxtLink to="/" class="btn btn-primary">Home</NuxtLink>
+        <div class="h-full w-40 bg-base-100 flex-shrink-0 p-4 flex flex-col gap-4">
+            <h2 class="text-xl font-bold">
+                Dev Dungeon
+            </h2>
+            <div class="flex flex-col gap-4 h-full">
+                <NuxtLink to="/home" class="btn btn-primary">Home</NuxtLink>
                 <NuxtLink to="/profile" class="btn btn-primary">Profile</NuxtLink>
                 <div class="grow"></div>
                 <button @click="logout()" class="btn btn-primary btn-outline">Logout</button>
