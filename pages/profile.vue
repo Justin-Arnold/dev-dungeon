901a7afcc5e503f13d8f9fee706bb6c27095f899
Justin Arnold <justin.arnold@programmer.net>
2024-02-11 18:53:41 -0500

    feat: Add more information to profile

--- pages/profile.vue
+++ pages/profile.vue
@@ -3,6 +3,7 @@ const user = useSupabaseUser()
 import type { Database } from '~/types/database.types'
 
 
+
 function addGithub() {
     console.log('adding github')
     navigateTo('https://github.com/apps/dev-dungeon-app/installations/new', {external: true})
@@ -14,6 +15,12 @@ const client = useSupabaseClient<Database>()
 
 const { data: githubIntegration, error } = useAsyncData(async () => {
     const { data } = await client.from('github_integration').select().eq('user_id', user.value?.id ?? '')
+
+    return data
+})
+
+const { data: profile, error: profileError } = useAsyncData(async () => {
+    const { data } = await client.from('profile').select().eq('user_id', user.value?.id ?? '')
 
     return data
 })
@@ -32,15 +39,63 @@ const githubData = computed(() => {
     return githubIntegration.value[0]
 })
 
+const userLevel = computed(() => {
+    if (!profile.value) {
+        return 'Level 1'
+    }
+    console.log('profile', profile.value[0].total_experience)
+    return `${levelFromExperience.value}`
+})
+
+function experienceForLevelEquation(L: number, baseXP: number = 100, growthRate: number = 0.1, cycleFactor: number = 2.0) {
+    const cycleMultiplier = 1 + ((Math.floor((L - 1) / 10)) / cycleFactor)
+    return Math.floor(baseXP * ((1 + growthRate) ** L) * cycleMultiplier)
+}
+
+
+const levelFromExperience = computed(() => {
+    let totalXp = profile.value?.[0]?.total_experience ?? 0
+    let accumulatedXp = 0
+    let level = 1
+    while (accumulatedXp <= totalXp) {
+        const xpForNextLevel = experienceForLevelEquation(level)
+        accumulatedXp += xpForNextLevel
+        if (accumulatedXp > totalXp) {
+            level !== 1 ? level-- : ''
+            break
+        } else {
+            level++
+        }
+    }
+    return level
+})
 </script>
 
 <template>
     <div class="flex flex-col gap-8">
-        <div>
-            <h1 class="text-3xl font-bold">Profile</h1>
-            <p class="">
-            Welcome, {{ user?.email }}
-            </p>
+        <div class="flex justify-between">
+            <div class="flex h-full gap-4">
+                <div class="grid place-items-center h-44 rounded bg-base-200 !aspect-square shadow-md">
+                    <div>
+                        <p class="font-light">Level</p>
+                        <p class="text-[100px] leading-none -mt-2">{{ userLevel }}</p>
+                    </div>
+                </div>
+                <div class="flex flex-col gap-2">
+                    <p class="text-xl text-semibold">{{ profile?.[0]?.display_name ?? '' }}</p>
+                    <div class="relative">
+                        <progress class="progress progress-error w-56 h-6 shadow-md" value="100" max="100"></progress>
+                        <p class="absolute top-0 left-1/2 z-2 text-black/60 dark:text-white/60 -translate-x-1/2">100 / 100 HP</p>
+                    </div>
+                    <div class="relative">
+                        <progress class="progress progress-primary w-56 h-6 shadow-md" value="23" max="100"></progress>
+                        <p class="absolute top-0 left-1/2 z-2 text-black/60 dark:text-white/60 -translate-x-1/2">23 / 100 XP</p>
+                    </div>
+                </div>
+            </div>
+            <div class="p-2 rounded bg-neutral/80">
+                <img src="~/assets/profile.webp" class="object-cover w-64 rounded"/>
+            </div>
         </div>
         <div class="flex flex-col gap-2">
             <h2 class="text-2xl">Integrations</h2>
